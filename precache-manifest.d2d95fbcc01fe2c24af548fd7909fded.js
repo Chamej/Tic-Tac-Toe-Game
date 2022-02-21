a015fe5e197773b70a76f583c0c96029a47e62a8
Chamej <chamej.awad@gmail.com>
2022-02-21 23:46:57 +0100

    Updates

---
+++ precache-manifest.d2d95fbcc01fe2c24af548fd7909fded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74717d5c338a02cfad5e69763149cfe0",
+    "url": "/Tic-Tac-Toe-Game/index.html"
+  },
+  {
+    "revision": "3a6dadad51c092d3a375",
+    "url": "/Tic-Tac-Toe-Game/static/css/main.beac62f7.chunk.css"
+  },
+  {
+    "revision": "38aa2bdf9e8314ff9d12",
+    "url": "/Tic-Tac-Toe-Game/static/js/2.7569c802.chunk.js"
+  },
+  {
+    "revision": "c9e0c53042442321331287fb1614b34d",
+    "url": "/Tic-Tac-Toe-Game/static/js/2.7569c802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8eb231cd6e0c514024",
+    "url": "/Tic-Tac-Toe-Game/static/js/3.14ac43e1.chunk.js"
+  },
+  {
+    "revision": "3a6dadad51c092d3a375",
+    "url": "/Tic-Tac-Toe-Game/static/js/main.14314e87.chunk.js"
+  },
+  {
+    "revision": "572095d6b3933bf10a13",
+    "url": "/Tic-Tac-Toe-Game/static/js/runtime-main.36c207b6.js"
+  }
+]);
